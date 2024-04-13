d2cb193848115daac6cd34fcbb3cd4212178bbe7
neel <neelbarvaliya04@gmail.com>
2024-04-13 19:33:49 +0530

    completed

---
+++ README.md
@@ -0,0 +1,31 @@
+# Frontend Mentor - Expenses chart component
+
+![Design preview for the Expenses chart component coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+This is a solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## The challenge
+
+Your challenge is to build out this bar chart component and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+We provide the data for the chart in a local `data.json` file. So you can use that to dynamically add the bars if you choose.
+
+Your users should be able to:
+
+- View the bar chart and hover over the individual bars to see the correct amounts for each day
+- See the current day's bar highlighted in a different colour to the other bars
+- View the optimal layout for the content depending on their device's screen size
+- See hover states for all interactive elements on the page
+- **Bonus**: See dynamically generated bars based on the data provided in the local JSON file
+
+### Links
+
+- Live Site URL: [🌐](https://neilneel.github.io/sunnyside-agency-landing-page-main/)
+
+## Author
+
+- Frontend Mentor - [@Neel Barvaliya](https://www.frontendmentor.io/profile/NeilNeel)
---
+++ script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+// DOM ELEMENTS
+const balanceEl = document.querySelector(".balance-container");
+const chartEl = document.querySelector(".chart-container");
+const ctx = document.getElementById("myChart");
+const date = new Date();
+
+// CHART JS API
+const chart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["mon", "tus", "wed", "thus", "fri", "sat", "sun"],
+    datasets: [
+      {
+        label: "$",
+        data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+        borderWidth: 1,
+        backgroundColor: (color) => {
+          let colors =
+            color.index === date.getDay() - 1
+              ? "rgba(118, 181, 188, 1)"
+              : "rgba(236, 119, 95, 1)";
+          return colors;
+        },
+        borderRadius: 5,
+        borderSkipped: false,
+        hoverBackgroundColor: (color) => {
+          //   const bg = color.element.options.backgroundColor;
+          let colors =
+            color.index === date.getDay() - 1
+              ? "rgba(118, 181, 188, 0.7)"
+              : "rgba(236, 119, 95, 0.7)";
+          return colors;
+        },
+      },
+    ],
+  },
+  options: {
+    aspectRatio: 2,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        suggestedMin: 5,
+        suggestedMax: 70,
+        display: false,
+        grid: {
+          color: "rgba(0, 0, 0, 0)",
+        },
+      },
+      x: {
+        grid: { color: "rgba(0, 0, 0, 0)" },
+        border: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+// LOADING EFFECT
+const revealElement = function (entries, observer) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("hidden");
+
+  if (entry.target && entry.target instanceof Element) {
+    observer.unobserve(entry.target);
+  } else {
+    console.error("Invalid entry target for observer");
+  }
+};
+
+const balObserver = new IntersectionObserver(revealElement, {
+  root: null,
+  threshold: 1.0,
+});
+
+const chartObserver = new IntersectionObserver(revealElement, {
+  root: null,
+  threshold: 1.0,
+});
+
+balObserver.observe(balanceEl);
+// chartObserver.observe(chartEl);
+setTimeout(() => chartObserver.observe(chartEl), 300);
